c3383024787be48d5372f124e595f8190b8231df
saparkhid <parkhidpage@gmail.com>
2025-08-11 00:07:30 +0330

    Feat: creates the main UI

---
+++ WebPSharp.UI/MainWindow.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace WebPSharp.UI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtInput = new ReaLTaiizor.Controls.TextBoxEdit();
+            label1 = new Label();
+            btnInput = new ReaLTaiizor.Controls.DreamButton();
+            thunderGroupBox1 = new ReaLTaiizor.Controls.ThunderGroupBox();
+            pictureBoxPreview = new PictureBox();
+            label2 = new Label();
+            txtOutput = new ReaLTaiizor.Controls.TextBoxEdit();
+            btnOutput = new ReaLTaiizor.Controls.DreamButton();
+            btnConvert = new ReaLTaiizor.Controls.Button();
+            label3 = new Label();
+            label4 = new Label();
+            notificationBox1 = new ReaLTaiizor.Controls.NotificationBox();
+            btnOpenOutputFolder = new ReaLTaiizor.Controls.AirButton();
+            headerLabel1 = new ReaLTaiizor.Controls.HeaderLabel();
+            spaceButton1 = new ReaLTaiizor.Controls.SpaceButton();
+            thunderGroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPreview).BeginInit();
+            SuspendLayout();
+            // 
+            // txtInput
+            // 
+            txtInput.BackColor = Color.Transparent;
+            txtInput.Font = new Font("Tahoma", 11F);
+            txtInput.ForeColor = Color.FromArgb(176, 183, 191);
+            txtInput.Image = null;
+            txtInput.Location = new Point(14, 80);
+            txtInput.MaxLength = 32767;
+            txtInput.Multiline = false;
+            txtInput.Name = "txtInput";
+            txtInput.ReadOnly = false;
+            txtInput.Size = new Size(277, 41);
+            txtInput.TabIndex = 9;
+            txtInput.TextAlignment = HorizontalAlignment.Left;
+            txtInput.UseSystemPasswordChar = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(14, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 21);
+            label1.TabIndex = 10;
+            label1.Text = "Input Image";
+            // 
+            // btnInput
+            // 
+            btnInput.ColorA = Color.FromArgb(31, 31, 31);
+            btnInput.ColorB = Color.FromArgb(41, 41, 41);
+            btnInput.ColorC = Color.FromArgb(51, 51, 51);
+            btnInput.ColorD = Color.FromArgb(0, 0, 0, 0);
+            btnInput.ColorE = Color.FromArgb(25, 255, 255, 255);
+            btnInput.ForeColor = Color.FromArgb(40, 218, 255);
+            btnInput.Location = new Point(297, 80);
+            btnInput.Name = "btnInput";
+            btnInput.Size = new Size(141, 40);
+            btnInput.TabIndex = 12;
+            btnInput.Text = "Pick";
+            btnInput.UseVisualStyleBackColor = true;
+            // 
+            // thunderGroupBox1
+            // 
+            thunderGroupBox1.BackColor = Color.Transparent;
+            thunderGroupBox1.BodyColorA = Color.FromArgb(26, 26, 26);
+            thunderGroupBox1.BodyColorB = Color.FromArgb(30, 30, 30);
+            thunderGroupBox1.BodyColorC = Color.FromArgb(46, 46, 46);
+            thunderGroupBox1.BodyColorD = Color.FromArgb(50, 55, 58);
+            thunderGroupBox1.Controls.Add(pictureBoxPreview);
+            thunderGroupBox1.ForeColor = Color.WhiteSmoke;
+            thunderGroupBox1.Location = new Point(444, 80);
+            thunderGroupBox1.Name = "thunderGroupBox1";
+            thunderGroupBox1.Size = new Size(141, 136);
+            thunderGroupBox1.TabIndex = 13;
+            thunderGroupBox1.Text = "Preview";
+            // 
+            // pictureBoxPreview
+            // 
+            pictureBoxPreview.Location = new Point(8, 31);
+            pictureBoxPreview.Name = "pictureBoxPreview";
+            pictureBoxPreview.Size = new Size(126, 95);
+            pictureBoxPreview.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBoxPreview.TabIndex = 0;
+            pictureBoxPreview.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(14, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 21);
+            label2.TabIndex = 14;
+            label2.Text = "Output Image";
+            // 
+            // txtOutput
+            // 
+            txtOutput.BackColor = Color.Transparent;
+            txtOutput.Font = new Font("Tahoma", 11F);
+            txtOutput.ForeColor = Color.FromArgb(176, 183, 191);
+            txtOutput.Image = null;
+            txtOutput.Location = new Point(14, 175);
+            txtOutput.MaxLength = 32767;
+            txtOutput.Multiline = false;
+            txtOutput.Name = "txtOutput";
+            txtOutput.ReadOnly = false;
+            txtOutput.Size = new Size(277, 41);
+            txtOutput.TabIndex = 15;
+            txtOutput.TextAlignment = HorizontalAlignment.Left;
+            txtOutput.UseSystemPasswordChar = false;
+            // 
+            // btnOutput
+            // 
+            btnOutput.ColorA = Color.FromArgb(31, 31, 31);
+            btnOutput.ColorB = Color.FromArgb(41, 41, 41);
+            btnOutput.ColorC = Color.FromArgb(51, 51, 51);
+            btnOutput.ColorD = Color.FromArgb(0, 0, 0, 0);
+            btnOutput.ColorE = Color.FromArgb(25, 255, 255, 255);
+            btnOutput.ForeColor = Color.FromArgb(40, 218, 255);
+            btnOutput.Location = new Point(297, 176);
+            btnOutput.Name = "btnOutput";
+            btnOutput.Size = new Size(141, 40);
+            btnOutput.TabIndex = 16;
+            btnOutput.Text = "Pick";
+            btnOutput.UseVisualStyleBackColor = true;
+            // 
+            // btnConvert
+            // 
+            btnConvert.BackColor = Color.Transparent;
+            btnConvert.BorderColor = Color.White;
+            btnConvert.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            btnConvert.EnteredColor = Color.FromArgb(32, 34, 37);
+            btnConvert.Font = new Font("Microsoft Sans Serif", 12F);
+            btnConvert.Image = null;
+            btnConvert.ImageAlign = ContentAlignment.MiddleLeft;
+            btnConvert.InactiveColor = Color.FromArgb(32, 34, 37);
+            btnConvert.Location = new Point(14, 242);
+            btnConvert.Name = "btnConvert";
+            btnConvert.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            btnConvert.PressedColor = Color.FromArgb(165, 37, 37);
+            btnConvert.Size = new Size(571, 40);
+            btnConvert.TabIndex = 17;
+            btnConvert.Text = "Convert";
+            btnConvert.TextAlignment = StringAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(14, 311);
+            label3.Name = "label3";
+            label3.Size = new Size(261, 21);
+            label3.TabIndex = 18;
+            label3.Text = "Developed by Seyed Ahmad Parkhid";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(14, 338);
+            label4.Name = "label4";
+            label4.Size = new Size(158, 21);
+            label4.TabIndex = 19;
+            label4.Text = "https://www.cafebit.ir";
+            // 
+            // notificationBox1
+            // 
+            notificationBox1.BorderCurve = 8;
+            notificationBox1.CloseForeColor = Color.Black;
+            notificationBox1.ErrorBackColor = Color.Crimson;
+            notificationBox1.ErrorBorderColor = Color.Crimson;
+            notificationBox1.ErrorForeColor = Color.White;
+            notificationBox1.ErrorTitleText = "ERROR";
+            notificationBox1.Font = new Font("Tahoma", 9F);
+            notificationBox1.Image = null;
+            notificationBox1.Location = new Point(185, 135);
+            notificationBox1.MinimumSize = new Size(100, 40);
+            notificationBox1.Name = "notificationBox1";
+            notificationBox1.NoticeBackColor = Color.Gray;
+            notificationBox1.NoticeBorderColor = Color.Gray;
+            notificationBox1.NoticeForeColor = Color.White;
+            notificationBox1.NoticeTitleText = "NOTICE";
+            notificationBox1.NotificationType = ReaLTaiizor.Controls.NotificationBox.Type.Notice;
+            notificationBox1.RoundCorners = true;
+            notificationBox1.ShowCloseButton = true;
+            notificationBox1.Size = new Size(232, 123);
+            notificationBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            notificationBox1.SuccessBackColor = Color.SeaGreen;
+            notificationBox1.SuccessBorderColor = Color.SeaGreen;
+            notificationBox1.SuccessForeColor = Color.White;
+            notificationBox1.SuccessTitleText = "SUCCESS";
+            notificationBox1.TabIndex = 20;
+            notificationBox1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            notificationBox1.WarningBackColor = Color.FromArgb(255, 128, 0);
+            notificationBox1.WarningBorderColor = Color.FromArgb(255, 128, 0);
+            notificationBox1.WarningForeColor = Color.White;
+            notificationBox1.WarningTitleText = "WARNING";
+            // 
+            // btnOpenOutputFolder
+            // 
+            btnOpenOutputFolder.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
+            btnOpenOutputFolder.Font = new Font("Segoe UI", 9F);
+            btnOpenOutputFolder.Image = null;
+            btnOpenOutputFolder.Location = new Point(236, 201);
+            btnOpenOutputFolder.Name = "btnOpenOutputFolder";
+            btnOpenOutputFolder.NoRounding = false;
+            btnOpenOutputFolder.Size = new Size(139, 24);
+            btnOpenOutputFolder.TabIndex = 22;
+            btnOpenOutputFolder.Text = "Open Output Folder";
+            btnOpenOutputFolder.Transparent = false;
+            // 
+            // headerLabel1
+            // 
+            headerLabel1.AutoSize = true;
+            headerLabel1.BackColor = Color.Transparent;
+            headerLabel1.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Bold);
+            headerLabel1.ForeColor = Color.FromArgb(255, 255, 255);
+            headerLabel1.Location = new Point(216, 9);
+            headerLabel1.Name = "headerLabel1";
+            headerLabel1.Size = new Size(167, 18);
+            headerLabel1.TabIndex = 23;
+            headerLabel1.Text = "SAP WebP Sharp 1.0";
+            // 
+            // spaceButton1
+            // 
+            spaceButton1.Customization = "Kioq/zIyMv8yMjL/Kioq/y8vL/8nJyf//v7+/yMjI/8qKir/";
+            spaceButton1.Font = new Font("Verdana", 8F);
+            spaceButton1.Image = null;
+            spaceButton1.Location = new Point(555, 9);
+            spaceButton1.Name = "spaceButton1";
+            spaceButton1.NoRounding = false;
+            spaceButton1.Size = new Size(30, 30);
+            spaceButton1.TabIndex = 24;
+            spaceButton1.Text = "X";
+            spaceButton1.TextAlignment = HorizontalAlignment.Center;
+            spaceButton1.Transparent = false;
+            spaceButton1.Click += this.spaceButton1_Click;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(597, 378);
+            Controls.Add(spaceButton1);
+            Controls.Add(headerLabel1);
+            Controls.Add(btnOpenOutputFolder);
+            Controls.Add(notificationBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(btnConvert);
+            Controls.Add(btnOutput);
+            Controls.Add(txtOutput);
+            Controls.Add(label2);
+            Controls.Add(thunderGroupBox1);
+            Controls.Add(label1);
+            Controls.Add(txtInput);
+            Controls.Add(btnInput);
+            Cursor = Cursors.Hand;
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1920, 1032);
+            MinimumSize = new Size(126, 50);
+            Name = "MainWindow";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "formTheme1";
+            TransparencyKey = Color.Fuchsia;
+            Load += Form1_Load;
+            thunderGroupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPreview).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ReaLTaiizor.Controls.TextBoxEdit txtInput;
+        private Label label1;
+        private ReaLTaiizor.Controls.DreamButton btnInput;
+        private ReaLTaiizor.Controls.ThunderGroupBox thunderGroupBox1;
+        private PictureBox pictureBoxPreview;
+        private Label label2;
+        private ReaLTaiizor.Controls.TextBoxEdit txtOutput;
+        private ReaLTaiizor.Controls.DreamButton btnOutput;
+        private ReaLTaiizor.Controls.Button btnConvert;
+        private Label label3;
+        private Label label4;
+        private ReaLTaiizor.Controls.NotificationBox notificationBox1;
+        private ReaLTaiizor.Controls.AirButton btnOpenOutputFolder;
+        private ReaLTaiizor.Controls.HeaderLabel headerLabel1;
+        private ReaLTaiizor.Controls.SpaceButton spaceButton1;
+    }
+}
---
+++ WebPSharp.UI/MainWindow.cs
@@ -0,0 +1,90 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace WebPSharp.UI
+{
+    public partial class MainWindow : Form
+    {
+        #region "WndProc Constants"
+        const int WM_NCHITTEST = 0x84;
+        const int HTCLIENT = 1;
+        const int HTCAPTION = 2;
+
+        #endregion 
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupForm();
+          
+        }
+
+        private void SetupForm()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.StartPosition = FormStartPosition.CenterScreen;
+           // this.Size = new Size(450, 300);
+            this.Padding = new Padding(0);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notificationBox1.NotificationType = ReaLTaiizor.Controls.NotificationBox.Type.Warning;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+          
+           base.OnPaint(e);
+           MakeTheWindowCornersRound();
+           FillFormBackColour(e);
+        }
+
+        private void spaceButton1_Click(object sender,EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MakeTheWindowCornersRound()
+        {
+            GraphicsPath path = new GraphicsPath();
+            int radius = 15;
+            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            this.Region = new Region(path);
+        }
+        private void FillFormBackColour(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                     this.ClientRectangle,
+                        Color.FromArgb(45, 45, 48),  // dark grey
+                        Color.FromArgb(28, 28, 28),  // almost black
+                     LinearGradientMode.Vertical))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+        
+            if (m.Msg == WM_NCHITTEST)
+            {
+                base.WndProc(ref m);
+
+
+                // If the mouse is in the "content" area
+                if ((int)m.Result == HTCLIENT)
+                {
+                    // Pretend it’s on the title bar , so user can drag the window
+                    m.Result = (IntPtr)HTCAPTION;
+                }
+                return;
+            }
+            base.WndProc(ref m);
+        }
+    }
+}
